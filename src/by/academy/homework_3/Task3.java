3e67f44ddac7d5cc91a01d0d32f4d13af8aa3241
e.nazarchuk <e.nazarchuk@gmail.com>
2020-11-10 20:53:12 +0300

    first

--- src/by/academy/homework_3/Task3.java
+++ src/by/academy/homework_3/Task3.java
@@ -1,19 +1,34 @@
 package by.academy.homework_3;
 import java.util.*;
-
-
 /*
-
+Ввести n слов с консоли.
+Найти слово, в котором число различных символов минимально.
+Если таких слов несколько, найти первое из них.
  */
-
 public class Task3 {
-    public static void main(String[] args) {
+    public static String Task3(String str, int n) {
+        //Scanner scan = new Scanner(System.in);
+        //System.out.print("Vvedite slovo1: ");
+        String s = "This is a sample sentence.";
+        /* перегруппировывает строку в массив слов */
+        String[] word = s.split(str, n);
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите любые 2 слова или фразу: ");
-        String phrase1 = sc.nextLine();
-        String phrase2 = sc.nextLine();
-        System.out.println(phrase1 + " " + phrase2);
-
+        /* вычисляет слово с минимальным кол-вом различных символов */
+        int count = 100; // минимальное число разлчных символов у слова word[i]
+        int iAns = 0; // индекс искомого слова в массиве word
+        for (int i = 0; i < word.length; i++) { // проход по словам у массива word
+            int curCount = 0; // минимальное число различных сивмолов у слова word[i]
+            for (int j = 0; j < word[i].length(); j++) { // проход по символам в слове word[i]
+                char tmp = (char) word[i].indexOf(j); // j-тый символ
+                if (word[i].indexOf(tmp) == j) {
+                    curCount++;
+                }
+            }
+            if (curCount < count) {
+                count = curCount;
+                iAns = i;
+            }
+        }
+        return word[iAns];
     }
 }
